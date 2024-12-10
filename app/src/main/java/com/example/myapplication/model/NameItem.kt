bf37d8d813e931878017cb7c6b8231a956212aa3
huutaquang@gmail.com <huutaquang@gmail.com>
2024-12-10 13:51:29 +0700

    add name enum

--- app/src/main/java/com/example/myapplication/model/NameItem.kt
+++ app/src/main/java/com/example/myapplication/model/NameItem.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.model
 
-enum class NameItem {
-
+enum class NameItem(val display: String) {
+    btSweater("Beautiful Sweater"),
+    tenisPant("Tenis SweatPant")
 }
